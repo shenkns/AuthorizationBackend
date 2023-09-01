ca4bdc438098855b2e63564213e6f231e663814e
shenkns <shenkns@gmail.com>
2023-09-02 02:59:29 +0300

    Remove DB connect

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const dbUrl = 'mongodb+srv://admin:TvyoPh4fSjE0tgAP@cluster0.qvainjk.mongodb.net
 const express = require('express');
 const app = express();
 
-const mongoose = require('mongoose');
-mongoose
-.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((res) => console.log("Connected to DB"))
-.catch((error) => console.log(error));
-
 app.get('/', function(request, response)
 {
     console.log(request.url);
