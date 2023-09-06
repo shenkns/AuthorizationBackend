a3ec40736b9430d26e82587e74d0ce725b709274
shenkns <shenkns@gmail.com>
2023-09-06 10:27:45 +0300

    Delete Session module

---
+++ src/user/auth/deleteSession.js
@@ -0,0 +1,8 @@
+// Listen /user/auth/delete-session path
+const deleteSession = async function(request, response) {
+
+};
+
+module.exports = deleteSession;
+
+console.log("Delete Session module initialized!");
